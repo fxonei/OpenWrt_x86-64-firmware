bfb2e92b6533636872ddbfa2c7d6e6160ec79c62
fxone <jhq502799617@gmail.com>
2023-04-05 21:57:49 +0800

    Update diy-part1.sh
    新增三方源

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,10 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+
+
+# Set default theme to luci-theme-argon
+##uci set luci.main.mediaurlbase='/luci-static/argon'
+
