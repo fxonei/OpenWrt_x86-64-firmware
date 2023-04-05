d79d888a48d4fbd036aceb4af802d523d9c7cce0
fxone <jhq502799617@gmail.com>
2023-04-05 21:59:21 +0800

    Update diy-part2.sh
    新增修改ip和名称

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,14 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.5/g' package/base-files/files/bin/config_generate
+
+
+# Add date version
+##export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
+##sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
+
+# Rename hostname to OpenWrt
+pushd package/base-files/files/bin
+sed -i 's/jhq_OpenWrt/OpenWrt/g' config_generate
+popd
